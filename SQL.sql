551c8d6720571522bd4a453f1068c894b3718560
lyubadd <112446117+lyubadd@users.noreply.github.com>
2022-08-30 17:11:19 -0400

    Create SQL.sql

---
+++ SQL.sql
@@ -0,0 +1,38 @@
+```
+INSERT INTO `leafy-star-345020.Cyclistic.202105_202204`
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_05`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_06`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_07`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_08`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_09`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_10`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_11`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2021_12`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2022_01`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2022_02`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2022_03`
+UNION ALL
+SELECT *
+FROM `leafy-star-345020.Cyclistic.2022_04`;
+```
