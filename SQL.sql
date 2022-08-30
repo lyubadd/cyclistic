c8353c1033dacbbaa2c5ab00bb9de062bd5a1a51
lyubadd <112446117+lyubadd@users.noreply.github.com>
2022-08-30 17:19:35 -0400

    Update SQL.sql

--- SQL.sql
+++ SQL.sql
@@ -1,3 +1,5 @@
+-- merge the 12 tables covering the 12 months of the past year.
+
 ```
 INSERT INTO `leafy-star-345020.Cyclistic.202105_202204`
 SELECT *
@@ -35,4 +37,17 @@ FROM `leafy-star-345020.Cyclistic.2022_03`
 UNION ALL
 SELECT *
 FROM `leafy-star-345020.Cyclistic.2022_04`;
+```
+
+-- check for duplicate rides
+
+```
+SELECT COUNT(DISTINCT ride_id)
+FROM  `leafy-star-345020.Cyclistic.202105_202204`;
+```
+-- check for any errors in all the columns, e.g. more than 3 types of rideable_types, or more than 2 values in member_casual.
+```
+SELECT
+DISTINCT rideable_type
+FROM `leafy-star-345020.Cyclistic.202105_202204`;
 ```
